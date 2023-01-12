f7632095fe980c82128b92d1335c38c7418d0bb4
jnsw <jns.w@icloud.com>
2023-01-12 18:43:01 +0800

    Semi-unassisted BinarySearch Solution

--- src/day2/BinarySearchList.ts
+++ src/day2/BinarySearchList.ts
@@ -1,7 +1,22 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-    // if im not wrong, binary search is the one where we split in two?
-    // there is a low and high, use that to find the mid
+  // if im not wrong, binary search is the one where we split in two?
+  // there is a low and high, use that to find the mid
 
-    let low = 0;
-    let high = haystack.length();
+  let low = 0;
+  let high = haystack.length;
+
+  while (low < high) {
+    let mid = Math.floor(low+(high-low)/2);
+
+    let val = haystack[mid];
+
+    if (needle === val) return true;
+
+    if (needle < val) {
+      high = mid;
+    } else if (needle > val) {
+      low = mid + 1;
+    }
+  }
+  return false;
 }
