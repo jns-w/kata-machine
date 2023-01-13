12a0713cb2c38cfde2be4d86fa56d83fb5cddb61
jns-w <jns.w@icloud.com>
2023-01-13 21:33:42 +0800

    Queue -- fixed solution

--- src/day2/Queue.ts
+++ src/day2/Queue.ts
@@ -1,18 +1,48 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.head = undefined;
+        this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        const node = { value: item } as Node<T>;
+        this.length++;
+        if (!this.tail) {
+            this.tail = node;
+            this.head = node;
 
-}
+            return;
+        }
+        this.tail.next = node;
+        this.tail = node;
+    }
+
     deque(): T | undefined {
+        if (!this.head) return undefined;
+        this.length--;
 
-}
+        const prevHead = this.head;
+        this.head = this.head.next;
+
+        prevHead.next = undefined; // breaking the link garbage
+
+        if (this.length === 0) {
+            this.tail = this.head = undefined;
+        }
+
+        return prevHead.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
