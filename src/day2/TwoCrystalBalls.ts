648ca4fa9234d59cd7681829a4633590d3807257
jns-w <jns.w@icloud.com>
2023-01-13 18:16:57 +0800

    Two Crystal Balls -- independant solution

--- src/day2/TwoCrystalBalls.ts
+++ src/day2/TwoCrystalBalls.ts
@@ -1,3 +1,26 @@
-export default function two_crystal_balls(breaks: boolean[]): number {
+// two crystal balls drop
+// find out where it breaks
+// the key is to find out its limit in the most optimised way
 
+export default function two_crystal_balls(breaks: boolean[]): number {
+    const len = breaks.length;
+    if (!len) return -1;
+
+    let i = 0;
+    let jump = Math.sqrt(len);
+
+    while (i < len) {
+        i = i + jump;
+        if (breaks[i]) break;
+    }
+
+    // now i is the end limit
+
+    let j = i - jump;
+
+    while (j < i) {
+        if (breaks[j]) return j;
+        j++;
+    }
+    return -1;
 }
