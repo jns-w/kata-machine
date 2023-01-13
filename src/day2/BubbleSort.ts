f4f3c92b4f50e4a56bc67d9d1afdaf802c8bf5ca
jns-w <jns.w@icloud.com>
2023-01-13 21:34:36 +0800

    Bubble Sort -- Independent soulution

--- src/day2/BubbleSort.ts
+++ src/day2/BubbleSort.ts
@@ -1,3 +1,15 @@
 export default function bubble_sort(arr: number[]): void {
-
+    // dont forget the optimisations, after each loop, last item is surely sorted.
+    for (let i = 0; i < arr.length - 1; ++i) {
+        let count = 0;
+        for (let j = 0; j < arr.length - 1 - i; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                count++;
+            }
+        }
+        if (count === 0) break;
+    }
 }
