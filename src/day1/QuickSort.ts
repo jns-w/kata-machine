f4f8a061db6807ad4c9b083c3552fa18ea127c8e
jns-w <jns.w@icloud.com>
2023-01-14 13:40:35 +0800

    Quick Sort updates / explanation / independent solution

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,65 +1,61 @@
 // divide and conquer
-// quick sort is recursive
+// quick sort is recursive and is memory efficient
 //
 // we pick a pivot point (index) in the array
 // iterate through the whole array and place items that are smaller than pivot value to front
 // and then after the iteration we place the pivot number at the end
 //
 // using the pivot as a splitter, we split the array into 2, and recurse on the subarrays (not inclusive of previous pivot)
-// 
+//
 // [7, 8, 6, 3, 4, 5] -- original array, we pick high index as pivot -- which is 5
 // [7, 8, 6, 3, 4, 5] -- 7 is larger, do nothing
 // [7, 8, 6, 3, 4, 5] -- 8 & 6 are larger, do nothing
 // [7, 8, 6, 3, 4, 5] -- 3 is smaller, so lets swap with i-0
-// [3, 8, 6, 7, 4, 5] -- 4 is smaller, so lets swap with i-1
-// [3, 4, 6, 7, 8, 5] -- now we need to move pivot to after last moved
+// [3, 8, 6, 7, 4, 5] -- 4 is smaller, so lets swap with i-1, & there is no more el left
+// [3, 4, 6, 7, 8, 5] -- now we need to move pivot to after last moved ie i-2
 // [3, 4, 5, 7, 8, 6] --- now we can recurse again
 // ---- end of first iteration
-// [3,4] [5] [7,8,6] 
+// [3,4] [5] [7,8,6]
 // [3] [4] [5] [6] [7, 8]
 // [3] [4] [5] [6] [7] [8]
-
 
 //
 // split array into subarrays
 
-
 function qs(arr: number[], lo: number, hi: number): void {
-  if (lo >= hi) {
-    return;
-  }
+    if (lo >= hi) {
+        return;
+    }
 
-  const pivotIdx = partition(arr, lo, hi);
+    const pivotIdx = partition(arr, lo, hi);
 
-  qs(arr, lo, pivotIdx-1);
-  qs(arr, pivotIdx+1, hi);
+    qs(arr, lo, pivotIdx - 1);
+    qs(arr, pivotIdx + 1, hi);
 }
 
 function partition(arr: number[], lo: number, hi: number): number {
-  const pivot = arr[hi];
+    const pivot = arr[hi];
 
-  let idx = lo - 1;
+    let idx = lo - 1;
 
-  for (let i = lo; i < hi; ++i) {
-    if (arr[i] <= pivot) {
-      idx++;
-      const tmp = arr[i];
-      arr[i] = arr[idx];
-      arr[idx] = tmp;
+    for (let i = lo; i < hi; ++i) {
+        if (arr[i] <= pivot) {
+            idx++;
+            const tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
+        }
     }
-  }
 
-  idx++;
-  arr[hi] = arr[idx];
-  arr[idx] = pivot;
-  
-  return idx;
+    idx++;
+    arr[hi] = arr[idx];
+    arr[idx] = pivot;
+
+    return idx;
 }
 
 export default function quick_sort(arr: number[]): void {
-
-  qs(arr, 0, arr.length-1);
-  
+    qs(arr, 0, arr.length - 1);
 }
 
 // PLAYGROUND
@@ -72,7 +68,7 @@ export default function quick_sort(arr: number[]): void {
 //   }
 //
 //   const pivotIndex = partition(arr, lo, hi);
-//   
+//
 //   qs(arr, lo, pivotIndex - 1);
 //   qs(arr, pivotIndex + 1, hi);
 //
--- src/day2/QuickSort.ts
+++ src/day2/QuickSort.ts
@@ -1,3 +1,33 @@
-export default function quick_sort(arr: number[]): void {
+const swap = (arr: Array<number>, i: number, j: number): void => {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+};
 
+const partition = (arr: Array<number>, low: number, high: number): number => {
+    const pivotVal = arr[high];
+    let ptr = low - 1;
+    for (let i = low; i < high; i++) {
+        if (arr[i] <= pivotVal) {
+            ptr++;
+            swap(arr, ptr, i);
+        }
+    }
+    ptr++;
+    swap(arr, ptr, high);
+    return ptr; // this is for next recursion pivot
+};
+
+const qs = (arr: Array<number>, low: number, high: number): void => {
+    // break case
+    if (low >= high) return;
+
+    const pivot = partition(arr, low, high);
+
+    qs(arr, low, pivot - 1);
+    qs(arr, pivot + 1, high);
+};
+
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
 }
