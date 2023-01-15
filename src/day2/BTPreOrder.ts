a03a5642acd1c72896d7a645f186d7d6a5087144
jns-w <jns.w@icloud.com>
2023-01-16 00:04:32 +0800

    Binary Tree Pre Order Traversal -- independent solution

--- src/day2/BTPreOrder.ts
+++ src/day2/BTPreOrder.ts
@@ -1,3 +1,21 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+// pre order traversal ---- head -> left -> right
+//
 
+function walk(
+    curr: BinaryNode<number> | null,
+    path: Array<number>,
+): Array<number> {
+    // break case
+    if (!curr) return path;
+
+    path.push(curr.value);
+
+    walk(curr.left, path);
+    walk(curr.right, path);
+
+    return path;
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
