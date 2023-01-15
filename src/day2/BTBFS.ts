8baa8c341eb41398c21a5719592c1f3dbe76f5e3
jns-w <jns.w@icloud.com>
2023-01-16 00:37:48 +0800

    BT BFS -- independent solution

--- src/day2/BTBFS.ts
+++ src/day2/BTBFS.ts
@@ -1,3 +1,17 @@
-export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+// binary tree BFS -- go layer by layer of the tree
+// use a queue to manage the sequencing
 
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    let q: Array<BinaryNode<number> | null> = [head];
+
+    while (q.length) {
+        const curr = q.shift();
+        if (!curr) continue;
+        if (curr.value === needle) return true;
+
+        if (curr.left) q.push(curr.left);
+        if (curr.right) q.push(curr.right);
+    }
+
+    return false;
 }
