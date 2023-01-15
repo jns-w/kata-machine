a906b7ada28bbc0cb12b4c9a2c7dfe7b922174c7
jns-w <jns.w@icloud.com>
2023-01-15 23:58:54 +0800

    Binary Tree In Order Traverse -- independent solution

--- src/day2/BTInOrder.ts
+++ src/day2/BTInOrder.ts
@@ -1,3 +1,22 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
+// in order traversal will go
+// deepest left start -- parent -- right, go up a level
+//    4
+//  2   6
+// 1 3 5 7
 
+function walk(
+    curr: BinaryNode<number> | null,
+    path: Array<number>,
+): Array<number> {
+    if (!curr) return path;
+
+    walk(curr.left, path);
+    path.push(curr.value);
+    walk(curr.right, path);
+
+    return path;
+}
+
+export default function in_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
