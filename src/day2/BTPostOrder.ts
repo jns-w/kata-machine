28b0ec5d3d5668110582f0bc3563efc180311565
jns-w <jns.w@icloud.com>
2023-01-16 00:03:44 +0800

    Binary Tree Post Order Traverse -- independent solution

--- src/day2/BTPostOrder.ts
+++ src/day2/BTPostOrder.ts
@@ -1,3 +1,20 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+// post order is
+// left and right first before parent node
+//
+function walk(
+    curr: BinaryNode<number> | null,
+    path: Array<number>,
+): Array<number> {
+    if (!curr) return path;
 
+    walk(curr.left, path);
+    walk(curr.right, path);
+
+    path.push(curr.value);
+
+    return path;
+}
+
+export default function post_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
