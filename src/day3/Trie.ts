adafd9402cca54b680a5128e49317aee03dbde10
jns-w <jns.w@icloud.com>
2023-01-18 00:08:25 +0800

    Second Trie -- independent solution

--- src/day3/Trie.ts
+++ src/day3/Trie.ts
@@ -1,12 +1,11 @@
 // type TrieNode = {
-//     children: { [key: string]: TrieNode };
+//     children?: { [key: string]: TrieNode };
 //     isEndOfWord?: boolean;
 // };
-//
-class TrieNode {
-    children: { [key: string]: TrieNode };
-    isEndOfWord?: boolean;
 
+class TrieNode {
+    public children: { [key: string]: TrieNode };
+    isEndOfWord?: boolean;
     constructor() {
         this.children = {};
         this.isEndOfWord = false;
@@ -14,71 +13,141 @@ class TrieNode {
 }
 
 export default class Trie {
-    private root: TrieNode;
-    public children: {};
+    public root: TrieNode;
 
     constructor() {
         this.root = new TrieNode();
-        this.children = {};
     }
 
     insert(item: string): void {
         let curr = this.root;
 
         for (let i = 0; i < item.length; i++) {
-            let next = curr.children[item[i]];
+            let next: TrieNode = curr.children[item[i]];
+
             if (!next) {
                 next = new TrieNode();
                 curr.children[item[i]] = next;
             }
+
             curr = next;
         }
 
         curr.isEndOfWord = true;
     }
+
     delete(item: string): void {
         let curr = this.root;
-
         for (let i = 0; i < item.length; i++) {
-            const next = curr.children[item[i]];
-            if (!next) {
-                return;
-            } else {
-                curr = next;
-            }
+            let next: TrieNode = curr.children[item[i]];
+
+            if (!next) return;
+
+            curr = next;
         }
-        if (curr.isEndOfWord) curr.isEndOfWord = false;
+        curr.isEndOfWord = false;
     }
 
     find(partial: string): string[] {
         let curr = this.root;
-        let output: Array<string> = [];
 
         for (let i = 0; i < partial.length; i++) {
-            const next = curr.children[partial[i]];
-            if (!next) {
-                return [];
-            } else {
-                curr = next;
-            }
+            let next: TrieNode = curr.children[partial[i]];
+            if (!next) return [];
+            curr = next;
         }
-        // we have reached the last char of search string
-        // we now have to start exploring the possible words
 
-        const s = partial;
-        this.walk(curr, s, output);
+        let output: Array<string> = [];
+        this.walk(curr, partial, output);
         return output;
     }
 
-    walk(node: TrieNode, s: string, arr: Array<string>) {
-        if (!node) return;
-
-        if (node.isEndOfWord) arr.push(s);
-        // for every child --- we walk
-        for (const [key, val] of Object.entries(node.children)) {
-            const next = val;
-            const newS = s.concat(key);
-            this.walk(next, newS, arr);
+    walk(curr: TrieNode, substring: string, output: Array<string>) {
+        if (!curr) return;
+        for (const [key, node] of Object.entries(curr.children)) {
+            let nextSubstring = substring.concat(key);
+            if (node.isEndOfWord) output.push(nextSubstring);
+            this.walk(node, nextSubstring, output);
         }
     }
 }
+
+// class TrieNode {
+//     children: { [key: string]: TrieNode };
+//     isEndOfWord?: boolean;
+//
+//     constructor() {
+//         this.children = {};
+//         this.isEndOfWord = false;
+//     }
+// }
+//
+// export default class Trie {
+//     private root: TrieNode;
+//     public children: {};
+//
+//     constructor() {
+//         this.root = new TrieNode();
+//         this.children = {};
+//     }
+//
+//     insert(item: string): void {
+//         let curr = this.root;
+//
+//         for (let i = 0; i < item.length; i++) {
+//             let next = curr.children[item[i]];
+//             if (!next) {
+//                 next = new TrieNode();
+//                 curr.children[item[i]] = next;
+//             }
+//             curr = next;
+//         }
+//
+//         curr.isEndOfWord = true;
+//     }
+//     delete(item: string): void {
+//         let curr = this.root;
+//
+//         for (let i = 0; i < item.length; i++) {
+//             const next = curr.children[item[i]];
+//             if (!next) {
+//                 return;
+//             } else {
+//                 curr = next;
+//             }
+//         }
+//         if (curr.isEndOfWord) curr.isEndOfWord = false;
+//     }
+//
+//     find(partial: string): string[] {
+//         let curr = this.root;
+//         let output: Array<string> = [];
+//
+//         for (let i = 0; i < partial.length; i++) {
+//             const next = curr.children[partial[i]];
+//             if (!next) {
+//                 return [];
+//             } else {
+//                 curr = next;
+//             }
+//         }
+//         // we have reached the last char of search string
+//         // we now have to start exploring the possible words
+//
+//         const s = partial;
+//         this.walk(curr, s, output);
+//         return output;
+//     }
+//
+//     walk(node: TrieNode, s: string, arr: Array<string>) {
+//         if (!node) return;
+//
+//         if (node.isEndOfWord) arr.push(s);
+//         // for every child --- we walk
+//         for (const [key, val] of Object.entries(node.children)) {
+//             const next = val;
+//             const newS = s.concat(key);
+//             this.walk(next, newS, arr);
+//         }
+//     }
+// }
